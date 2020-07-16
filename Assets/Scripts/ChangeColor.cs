085fdd8e8286a2218d347b59306f84ed0449502f
badir <badir.al-sayyed@stud-hshl.de>
2020-07-16 20:44:21 +0200

    HandDetector
    kinda working

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material handColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handColor.color = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
