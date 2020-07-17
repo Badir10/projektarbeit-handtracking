f336b46e7868580484bef3c9d75ea40fea5e929f
badir <badir.al-sayyed@stud-hshl.de>
2020-07-17 09:32:37 +0200

    Rigidbodies

--- Assets/Scripts/GestureEvents.cs
+++ Assets/Scripts/GestureEvents.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeColor : MonoBehaviour
+public class GestureEvents : MonoBehaviour
 {
     public List<GameObject> prefabs;
     void Start()
