b4a6b260493f4213886347d1dfdfe3f2bafde523
badir <badir.al-sayyed@stud-hshl.de>
2020-08-11 03:00:39 +0200

    TestUmgebung
    Testumgebung mit with Tracked Gestures

--- Assets/Scripts/GestureEvents.cs
+++ Assets/Scripts/GestureEvents.cs
@@ -8,6 +8,7 @@ public class GestureEvents : MonoBehaviour
     public List<GameObject> prefabs;
     public GameObject cube;
     public List<Color> colors;
+    public List<SpriteRenderer> gestureSprites;
 
     public void Spawn(int index)
     {
@@ -17,7 +18,37 @@ public class GestureEvents : MonoBehaviour
     public void ChangeColor(int index)
     {
         cube.GetComponent<Renderer>().material.color = colors[index];
+        
+    }
+
+    public void ChangeColorBack()
+    {
+        gestureSprites[0].color = Color.black;
+        gestureSprites[1].color = Color.black;
+        gestureSprites[2].color = Color.black;
+        gestureSprites[3].color = Color.black;
+        gestureSprites[4].color = Color.black;
+        gestureSprites[5].color = Color.black;
+        gestureSprites[6].color = Color.black;
     }
     
-    
+    public void GestureRecognized(int index)
+    {
+        gestureSprites[index].color = Color.white;
+        Invoke("ChangeColorBack", 1);
+        
+        
+        //gestureSprites[index].material.SetColor("_Color", Color.white);
+
+        /*if (IsInvoking())
+        {
+            //gestureSprites[index].color = Color.white;
+            gestureSprites[index].material.SetColor("_Color", Color.white);
+        }
+
+        if (!IsInvoking())
+        {
+            gestureSprites[index].material.SetColor("_Color", Color.black);
+        }*/
+    }
 }
---
+++ Assets/Scripts/LerpToController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LerpToController : MonoBehaviour
+{
+    public GameObject rightController;
+    private GameObject table;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            gameObject.transform.position = rightController.transform.position;
+            gameObject.transform.position -= new Vector3(0, 0.07f, 0);
+        }
+    }
+}
