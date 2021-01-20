68994578531aedb367904e0afc2b81479aa1a116
badir <badir.al-sayyed@stud-hshl.de>
2021-01-20 09:12:39 +0100

    HideHands works better now
    Used PrimaryHandtrigger instead of istracked as the "else-Statement"

--- Assets/Scripts/HideHands.cs
+++ Assets/Scripts/HideHands.cs
@@ -6,15 +6,9 @@ public class HideHands : MonoBehaviour
 {
     [SerializeField] private OVRHand handscript;
 
-    [SerializeField] private GameObject trackedHandL;
-    [SerializeField] private GameObject trackedHandR;
-    [SerializeField] private GameObject handL;
-    [SerializeField] private GameObject handR;
-
     [SerializeField] private GameObject lefthandAnchor;
     [SerializeField] private GameObject righthandAnchor;
 
-    public bool istracked;
     
     // Start is called before the first frame update
     void Start()
@@ -32,22 +26,15 @@ public class HideHands : MonoBehaviour
 
             lefthandAnchor.transform.GetChild(4).gameObject.SetActive(false);
             righthandAnchor.transform.GetChild(4).gameObject.SetActive(false);
-            
-            istracked = true;
         }
 
         if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0)
-        {
-            Debug.Log("MAMASITA");
-        }
         {
             lefthandAnchor.transform.GetChild(4).gameObject.SetActive(true);
             righthandAnchor.transform.GetChild(4).gameObject.SetActive(true);
             
             lefthandAnchor.transform.GetChild(3).gameObject.transform.GetChild(0).gameObject.SetActive(false);
             righthandAnchor.transform.GetChild(3).gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            
-            istracked = false;
         }
     }
 }
